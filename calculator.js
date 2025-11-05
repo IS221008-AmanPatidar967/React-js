fac80cfb7c3b7a5a55a6974c5e38bc69cae4599a
Aman Patidar <amandawariya0505@gmail.com>
2025-11-05 11:26:12 +0530

    calculator.js

---
+++ calculator.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import "./Calculator.css";
+
+function Calculator() {
+  const [input, setInput] = useState("");
+
+  const handleClick = (value) => {
+    setInput(input + value);
+  };
+
+  const handleClear = () => {
+    setInput("");
+  };
+
+  const handleBackspace = () => {
+    setInput(input.slice(0, -1));
+  };
+
+  const handleEqual = () => {
+    try {
+      // eslint-disable-next-line no-eval
+      setInput(eval(input).toString());
+    } catch (error) {
+      setInput("Error");
+    }
+  };
+
+  return (
+    <div className="calc-container">
+      <h4>CASIO</h4>
+      <h2>📱 Simple Calculator</h2>
+      <input type="text" value={input} readOnly className="calc-display" />
+
+      <div className="calc-buttons">
+        <button onClick={handleClear} className="clear">
+          C
+        </button>
+        <button onClick={handleBackspace}>⌫</button>
+        <button onClick={() => handleClick("%")}>%</button>
+        <button onClick={() => handleClick("/")}>÷</button>
+
+        <button onClick={() => handleClick("7")}>7</button>
+        <button onClick={() => handleClick("8")}>8</button>
+        <button onClick={() => handleClick("9")}>9</button>
+        <button onClick={() => handleClick("*")}>×</button>
+
+        <button onClick={() => handleClick("4")}>4</button>
+        <button onClick={() => handleClick("5")}>5</button>
+        <button onClick={() => handleClick("6")}>6</button>
+        <button onClick={() => handleClick("-")}>−</button>
+
+        <button onClick={() => handleClick("1")}>1</button>
+        <button onClick={() => handleClick("2")}>2</button>
+        <button onClick={() => handleClick("3")}>3</button>
+        <button onClick={() => handleClick("+")}>+</button>
+
+        <button onClick={() => handleClick("0")} className="zero">
+          0
+        </button>
+        <button onClick={() => handleClick(".")}>.</button>
+        <button onClick={handleEqual} className="equal">
+          =
+        </button>
+      </div>
+      <h6>casio 999</h6>
+    </div>
+    
+  );
+}
+
+export default Calculator;
